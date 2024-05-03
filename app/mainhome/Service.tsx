203168f8ac69bb8f261062325aab578aa52b39cc
Anezz12 <harsenaargrtya1@gmail.com>
2024-05-03 20:30:44 +0700

    update

--- app/mainhome/Service.tsx
+++ app/mainhome/Service.tsx
@@ -3,29 +3,28 @@
 import Image from "next/image";
 import React from "react";
 import { WobbleCard } from "@/components/ui/wobble-card";
+import Title from "@/components/ui/Title";
 function Service() {
   return (
     <div>
       <div>
-        <span>
-          <h2 className="text-3xl font-bold mb-4 text-center py-20">
-            Layanan Kami
-            <em className="text-blue-500 underline underline-offset-8">
-              {" "}
-              Di Mistar
-            </em>
-          </h2>
-        </span>
+        <div className=" py-20">
+          <Title
+            text="Layanan Kami"
+            className="flex flex-col items-center justify-center  cursor-pointer"
+          />
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
           <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
             <div className="max-w-sm">
               <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-                Signup for blazing-fast cutting-edge state of the art Gippity AI
-                wrapper today!
+                Penjilidan
               </h2>
               <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-                With over 100,000 mothly active bot users, Gippity AI is the
-                most popular AI platform for developers.
+                Melayani penjilidan Softcover, Hardcover, Langsung/Biasa dan
+                Mika. Dengan peralatan lengkap kami dan tim kami tidak perlu
+                diragukan lagi dengan hasilnya. Kami siap melayani dengan cepat
+                dan rapi
               </p>
             </div>
             <Image
@@ -33,45 +32,73 @@ function Service() {
               width={1000}
               height={1000}
               alt="linear demo image"
-              className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+              className="absolute -right-0 md:-right-[40%] lg:-right-[20%] -bottom-5 object-contain"
+            />
+          </WobbleCard>
+
+          <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+            <div className="max-w-sm">
+              <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+                Fotocopy & Print
+              </h2>
+              <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+                Secara umum, pengertian fotocopy adalah metode membuat tiruan
+                atau salinan dokumen pada kertas
+              </p>
+            </div>
+            <Image
+              src="/banner.jpeg"
+              width={1000}
+              height={1000}
+              alt="linear demo image"
+              className="absolute -right-0  md:-right-[40%] lg:-right-[20%] -bottom-3 object-contain "
             />
           </WobbleCard>
           <WobbleCard containerClassName="col-span-1 min-h-[300px]">
             <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-              No shirt, no shoes, no weapons.
+              Cetak E-Book
             </h2>
             <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-              If someone yells “stop!”, goes limp, or taps out, the fight is
-              over.
+              Buku elektronik buku elektronik atau e-book adalah publikasi buku
+              yang tersedia dengan wujud digital. Yang mana di dalamnya terdiri
+              dari gambar, teks, atau bahkan keduanya. Serta bisa dibuka dan
+              dibaca melalui layar smartphone, komputer, atau beberapa perangkat
+              elektronik lainnya.
             </p>
           </WobbleCard>
           <WobbleCard containerClassName="col-span-1 min-h-[300px]">
             <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-              No shirt, no shoes, no weapons.
+              Selain fotokopi kami juga melayani
             </h2>
             <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-              If someone yells “stop!”, goes limp, or taps out, the fight is
-              over.
+              1. Fotocopy <br />
+              2. Print A4,F4,A3 <br />
+              3. Print Boklet <br />
+              4. Print E-Book <br />
+              5. Print Hvs Warna <br /> 6. Cetak Foto/Edit Foto
             </p>
           </WobbleCard>
           <WobbleCard containerClassName="col-span-1 min-h-[300px]">
             <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-              No shirt, no shoes, no weapons.
+              Cetak Notebook/Blocknote
             </h2>
             <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-              If someone yells “stop!”, goes limp, or taps out, the fight is
-              over.
+              Buku catatan merupakan buku dengan isi yang dipakai untuk
+              mencatat. Bagi Anda yang sering menggelar atau menghadiri kegiatan
+              seminar, workshop maupun acara lainnya, pasti sudah tidak asing
+              lagi dengan souvenir buku catatan, yakni yang sering dijumpai
+              adalah block note.
             </p>
           </WobbleCard>
           <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
             <div className="max-w-sm">
               <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-                Signup for blazing-fast cutting-edge state of the art Gippity AI
-                wrapper today!
+                Pres, Laminating, Scan
               </h2>
               <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-                With over 100,000 mothly active bot users, Gippity AI is the
-                most popular AI platform for developers.
+                Kami melayani pres, laminating, dan scan. Dengan peralatan
+                lengkap kami dan tim kami tidak perlu diragukan lagi dengan
+                hasilnya. Kami siap melayani dengan cepat dan rapi
               </p>
             </div>
             <Image
@@ -79,7 +106,7 @@ function Service() {
               width={1000}
               height={1000}
               alt="linear demo image"
-              className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+              className="absolute -right-0  md:-right-[40%] lg:-right-[20%] -bottom-3 object-contain "
             />
           </WobbleCard>
         </div>
---
+++ components/ui/Title.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Title({ text, className }: { text: string; className?: string }) {
+  return (
+    <div className={className}>
+      <h1 className="text-3xl font-bold hover:text-blue-500 transition-all">
+        {text}
+      </h1>
+      <div className="w-40 h-2 bg-blue-500"></div>
+      <div className="w-40 h-2 bg-green-500 translate-x-2"></div>
+    </div>
+  );
+}
+
+export default Title;
